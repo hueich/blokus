149d3c74c8a23eb005581b20c6b66cb4398c198d
Addison Luh <addison.luh@ucla.edu>
2015-12-12 23:39:03 -0800

    Added model for Game and logic for creating new game and adding players.

--- game.go
+++ game.go
@@ -1,5 +1,56 @@
 package blokus
 
-const (
-	defaultBoardSize = 20
+import (
+	"fmt"
 )
+
+const (
+	// Default game board size.
+	DefaultBoardSize = 20
+	maxBoardSize     = 100
+)
+
+type Game struct {
+	id      int
+	players []*Player
+	board   *Board
+	// Set of pieces every player starts with.
+	pieceSet   []*Piece
+	curPieceID int
+}
+
+func NewGame(id int, size int, pieces []*Piece) (*Game, error) {
+	if size <= 0 || size > maxBoardSize {
+		return nil, fmt.Errorf("Board size must be between 1 and %v. Provided: %v", maxBoardSize, size)
+	}
+	return &Game{
+		id:         id,
+		board:      NewBoard(size),
+		pieceSet:   pieces,
+		curPieceID: 1,
+	}, nil
+}
+
+func (g *Game) genPieceID() int {
+	// TODO: Implement locking or use database to keep track of IDs
+	id := g.curPieceID
+	g.curPieceID += 1
+	return id
+}
+
+func (g *Game) AddPlayer(name string, color int, corner Coord) error {
+	// TODO: Check corner
+	p := &Player{
+		name:   name,
+		color:  color,
+		corner: corner,
+	}
+	// Make a copy of the set of starting pieces of the game.
+	for _, ps := range g.pieceSet {
+		b := make([]Coord, len(ps.blocks))
+		copy(b, ps.blocks)
+		p.pieces = append(p.pieces, NewPiece(g.genPieceID(), p, b))
+	}
+	g.players = append(g.players, p)
+	return nil
+}
---
+++ game_test.go
@@ -0,0 +1,78 @@
+package blokus
+
+import (
+	"testing"
+)
+
+func TestNewGame(t *testing.T) {
+	p := &Piece{blocks: []Coord{Coord{3, 4}}}
+	g, err := NewGame(123, 22, []*Piece{p})
+	if err != nil {
+		t.Fatalf("NewGame(): got %v, want no error", err)
+	}
+
+	if got, want := g.id, 123; got != want {
+		t.Errorf("Game id: got %v, want %v", got, want)
+	}
+	if g.board == nil {
+		t.Fatal("Game board: got nil, want not nil")
+	}
+	if got, want := len(g.board.grid), 22; got != want {
+		t.Errorf("Game board size: got %v, want %v", got, want)
+	}
+	if got, want := len(g.pieceSet), 1; got != want {
+		t.Fatalf("Game pieces len: got %v, want %v", got, want)
+	}
+	if got, want := len(g.pieceSet[0].blocks), 1; got != want {
+		t.Fatalf("Game pieces[0] blocks len: got %v, want %v", got, want)
+	}
+	if got, want := g.pieceSet[0].blocks[0], (Coord{3, 4}); got != want {
+		t.Errorf("Game pieces[0] blocks[0]: got %v, want %v", got, want)
+	}
+}
+
+func TestAddPlayer(t *testing.T) {
+	ps := []*Piece{
+		{blocks: []Coord{Coord{3, 4}}},
+		{blocks: []Coord{Coord{5, 6}}},
+	}
+	g, err := NewGame(123, 22, ps)
+	if err != nil {
+		t.Fatalf("NewGame(): got %v, want no error", err)
+	}
+
+	if err := g.AddPlayer("foo", Red, Coord{-1, -1}); err != nil {
+		t.Fatalf("AddPlayer(): got %v, want no error", err)
+	}
+
+	if got, want := len(g.players), 1; got != want {
+		t.Fatalf("Game players len: got %v, want %v", got, want)
+	}
+	p := g.players[0]
+	if got, want := p.name, "foo"; got != want {
+		t.Errorf("Player name: got %v, want %v", got, want)
+	}
+	if got, want := p.color, Red; got != want {
+		t.Errorf("Player color: got %v, want %v", ColorName(got), ColorName(want))
+	}
+	if got, want := p.corner, (Coord{-1, -1}); got != want {
+		t.Errorf("Player corner: got %v, want %v", got, want)
+	}
+	if got, want := len(p.pieces), 2; got != want {
+		t.Fatalf("Player pieces len: got %v, want %v", got, want)
+	}
+	if id0, id1 := p.pieces[0].id, p.pieces[1].id; id0 == id1 {
+		t.Errorf("Player pieces ids: got %v == %v, want not equal", id0, id1)
+	}
+
+	if got, want := len(p.pieces[0].blocks), 1; got != want {
+		t.Fatalf("Player pieces[0] blocks len: got %v, want %v", got, want)
+	}
+	if got, want := p.pieces[0].blocks[0], (Coord{3, 4}); got != want {
+		t.Errorf("Player pieces[0] blocks[0]: got %v, want %v", got, want)
+	}
+	p.pieces[0].Rotate()
+	if got, want := g.pieceSet[0].blocks[0], (Coord{3, 4}); got != want {
+		t.Errorf("Game pieces[0] blocks[0] after rotating player's piece: got %v, want %v", got, want)
+	}
+}
