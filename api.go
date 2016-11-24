23435122ca617c69a524ef86b5c164aa4b35a573
Addison Luh <hueich@gmail.com>
2016-11-23 21:38:40 -0800

    Tweaked API and added descriptions.

--- api.go
+++ api.go
@@ -1,32 +1,40 @@
 package blokus
 
 import (
-	"golang.org/x/net/context"
+	"context"
 )
 
 type GameOptions struct {
-	// ID of the starting set of pieces to use for the game. 0 is the default set.
-	PieceSet int
+	// Name of the set of starting pieces to use for the game. Leave empty for default set.
+	PieceSet string
 }
 
 // CreateGame returns the ID of the created game.
-func CreateGame(ctx context.Context, username, gamename string, size int, opt GameOptions) (int64, error) {
+// Username is the user who owns the created game.
+// BoardSize is the length of one edge of the board for a square board.
+func CreateGame(ctx context.Context, username, gamename string, boardSize int, opt *GameOptions) (int64, error) {
 	return 123, nil
 }
 
+// AddPlayer adds a new player to the game. It's an error to add an existing player or if the color or corner is taken.
 func AddPlayer(ctx context.Context, gameID int64, username string, color, corner int) error {
 	return nil
 }
 
-func StartGame(ctx context.Context, gameID int64) error {
+// StartGame starts the game with the specified player. It's an error to call this if the game already started.
+func StartGame(ctx context.Context, gameID int64, username string) error {
 	return nil
 }
 
+// PlacePiece places a piece on the board, given the position and orientation.
+// Coordinate [0,0] is the bottom left corner of the board. X increases rightward and Y increases upward.
+// Rotation starts from 0 and increases clockwise in 90-degree increments. Flip, if true, flips the piece horizontally, i.e. around the Y axis.
 func PlacePiece(ctx context.Context, gameID, pieceID int64, x, y, rot int, flip bool) error {
 	return nil
 }
 
+// GetGameState gets the current state of the game, from which a client may construct a view of the game.
 // TODO: Figure out what to return to concisely represent game state
-func GetGameState(ctx context.Context, gameID, lastPieceID int64) error {
+func GetGameState(ctx context.Context, gameID int64) error {
 	return nil
 }
