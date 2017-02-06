e7a6a02de12a1120f751f3a8027be1b961648f25
Addison Luh <hueich@gmail.com>
2017-02-05 16:23:57 -0800

    Populate default pieces

--- defaults.go
+++ defaults.go
@@ -9,5 +9,146 @@ const (
 func DefaultPieces() []*Piece {
 	pieces := make([]*Piece, 0)
 
+	// 1 block pieces.
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+	}))
+
+	// 2 block pieces.
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+	}))
+
+	// 3 block pieces.
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{1, 1},
+	}))
+
+	// 4 block pieces.
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{3, 0},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{2, 1},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{1, 1},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{1, 1},
+		{0, 1},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{1, 1},
+		{2, 1},
+	}))
+
+	// 5 block pieces.
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{3, 0},
+		{4, 0},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{3, 0},
+		{3, 1},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{2, 1},
+		{3, 1},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{1, 1},
+		{2, 1},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{0, 1},
+		{2, 1},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{3, 0},
+		{2, 1},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{2, 1},
+		{2, -1},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{2, 1},
+		{2, 2},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{1, 1},
+		{2, 1},
+		{2, 2},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{1, 1},
+		{1, 2},
+		{2, 2},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{1, 1},
+		{1, 2},
+		{2, 1},
+	}))
+	pieces = append(pieces, NewPieceOrNil([]Coord{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{1, 1},
+		{1, -1},
+	}))
+
 	return pieces
 }
