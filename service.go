6b5f8719c4b42e4467dbcbb1632ff32026944317
Addison Luh <hueich@gmail.com>
2017-02-19 20:37:22 -0800

    Working usage of go templates

--- service.go
+++ service.go
@@ -2,24 +2,49 @@ package blokusWebUI
 
 import (
 	"fmt"
+	"go/build"
+	"html/template"
+	"log"
 	"net/http"
+	"os"
 	"path"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 )
 
+const (
+	importDir    = "github.com/hueich/blokus-web-ui"
+	templatesDir = "templates"
+)
+
 type UIService struct {
-	apiURL string
+	apiURL   string
+	tmplsDir string
 }
 
-func NewService(r *mux.Router, apiURL string) (*UIService, error) {
+func NewService(r *mux.Router, apiURL, templatesDir string) (*UIService, error) {
 	if r == nil {
 		return nil, fmt.Errorf("Router cannot be nil")
 	}
 	if apiURL == "" {
-		return nil, fmt.Errorf("API path cannot be empty")
+		return nil, fmt.Errorf("API URL cannot be empty")
 	}
-	s := &UIService{apiURL: apiURL}
+	if templatesDir == "" {
+		if dir, err := getTemplatesDir(); err != nil {
+			return nil, err
+		} else {
+			templatesDir = dir
+		}
+	} else if !isReadableDir(templatesDir) {
+		return nil, fmt.Errorf("Could not read templates directory: %v", templatesDir)
+	}
+
+	log.Println("Using templates directory:", templatesDir)
+	s := &UIService{
+		apiURL:   apiURL,
+		tmplsDir: templatesDir,
+	}
 	s.addRoutes(r)
 	return s, nil
 }
@@ -36,6 +61,13 @@ func (s *UIService) addRoutes(r *mux.Router) {
 }
 
 func (s *UIService) getGamesHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles(filepath.Join(s.tmplsDir, "games-list.gohtml"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Could not parse template files: %v\n", err)
+		return
+	}
+
 	// TODO: Move this to use html templates
 	w.Header().Set("Content-Type", "text/html;charset=utf-8")
 	w.Write([]byte("<html><head></head><body>\n"))
@@ -44,10 +76,34 @@ func (s *UIService) getGamesHandler(w http.ResponseWriter, r *http.Request) {
 					  <button type="submit">Create Game</button>
 					</form>`, path.Join(s.apiURL, "games"))))
 
+	t.ExecuteTemplate(w, "games-list", []int64{123, 456})
+
 	// TODO: Add client side game fetching code
 	w.Write([]byte("</body></html>"))
 }
 
 func (s *UIService) getGameHandler(w http.ResponseWriter, r *http.Request) {
 	// TODO: Page to show game UI
+}
+
+func isReadableDir(dir string) bool {
+	f, err := os.Open(dir)
+	if err != nil {
+		return false
+	}
+	defer f.Close()
+	if info, err := f.Stat(); err != nil || !info.IsDir() {
+		return false
+	}
+	return true
+}
+
+func getTemplatesDir() (string, error) {
+	for _, sdir := range build.Default.SrcDirs() {
+		tdir := filepath.Join(sdir, importDir, templatesDir)
+		if isReadableDir(tdir) {
+			return tdir, nil
+		}
+	}
+	return "", fmt.Errorf("Could not find viable templates directory")
 }
